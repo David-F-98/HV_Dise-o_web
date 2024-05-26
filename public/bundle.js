3913775b33c18d673b4624f6b1f50565d7eb3790
David-F-98 <u2903149@unimilitar.edu.co>
2024-05-25 21:54:21 -0500

    estilos Boostrap & Sass

--- public/bundle.js
+++ public/bundle.js
@@ -121,9 +121,6 @@ const suelta = (e) =>{
     clickPresionado = false;
 };
 
-slider.addEventListener('mousedown', presiona);
-slider.addEventListener('mousemove', mueve);
-slider.addEventListener('mouseup', suelta);
 
 const botonesEmail = document.querySelectorAll('[data-action="abrir-ventana-correo"]');
 const ventanaCorreo = document.getElementById('ventana-correo');
@@ -177,5 +174,5 @@ window.addEventListener('load', async()=>{
     await animarTexto(document.querySelector('.hero__titulo--dos'));
     
     document.querySelectorAll('.hero__burbuja')[0].classList.add('hero__burbuja--active-1');
-    document.querySelectorAll('.hero__burbuja')[1].classList.add('hero__burbuja--active-2');
+    
 });
